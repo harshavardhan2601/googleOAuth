95234204a577e146effd1995f88053253dbf14b6
harshavardhan26 <harsha1679@gmail.com>
2020-08-11 12:08:45 +0530

    changes

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,31 @@ router.get('/google/login',passport.authenticate('google',{
   res.render('dashboard');
 });
 
+//login session//
+router.post('/page', function (req, res, next) {
+  var reqs = req.body;
+  console.log(reqs);
+  mongoose.model('User').findOne({ email: reqs.email, password: reqs.password })
+    .then((Obj) => {
+      console.log(Obj);
+      if (Obj) {
+        console.log(Obj._id);
+        req.session.user_id = Obj._id;
+        req.session.email_id = Obj.email;
+        console.log('_id======' + req.session.user_id);
+        console.log('_id======' + req.session.email_id);
+        // console.log({ status: 1, massage: 'Success' });
+        // res.send({ status: 1, 'data': Obj });
+        // res.json({ 'data': Obj});
+        res.redirect('dashboard');
+      } else {
+        res.send({ status: 2, massage: 'Failure' });
+      }
+
+    })
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('welcome', { title: 'Express' });
@@ -49,11 +74,11 @@ router.post('/signup',function (req,res,next) {
   }
 });
 
-// router.post('/dashboard',function (req,res,next) {
-//   res.render('dashboard');
-// });
+router.post('/dashboard',function (req,res,next) {
+  res.render('dashboard');
+});
 
-router.get('/dashboard', function (req, res, next) {
+router.get('/profile', function (req, res, next) {
   var s = req.session.user_id
   console.log(s);
   mongoose.model('User').find({ _id:s })
